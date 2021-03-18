6f0a9230e553a4b614f648ce01becd4e3dcc809d
Hieu Tran <tr.kimhieu@gmail.com>
2021-03-18 10:47:33 +0700

    feat: Add Baking/Cooking Ingredients products

--- Allsop.ShoppingCart.Microservices/Service.API.Catalog/Infrastructure/CatalogDbContext.cs
+++ Allsop.ShoppingCart.Microservices/Service.API.Catalog/Infrastructure/CatalogDbContext.cs
@@ -33,7 +33,12 @@ namespace Service.API.Catalog.Infrastructure
                 
                 new { Id = "77e63e97-b41a-4882-9574-52a3738fd93f",Name = "Coca-Cola",               Packaging = "6 x 2L",           OldPriceValue = 8.50m,  PriceValue = 8.30m,     PriceUnit = "GBP" , InventoryQuantity = 6,      Sku="DR-000001", CategoryId = "737c9710-e069-436a-a236-660e8277dedf" },
                 new { Id = "d2fb685f-86dd-47ac-bb23-8ffd8ce84941",Name = "Still Mineral Water",     Packaging = "6 x 24 x 500ml",                           PriceValue = 21.75m,    PriceUnit = "GBP" , InventoryQuantity = 9,      Sku="DR-000002", CategoryId = "737c9710-e069-436a-a236-660e8277dedf" },
-                new { Id = "bf7b5f89-a569-4e3d-acb8-04a07e1e1130",Name = "Sparkling Mineral Water", Packaging = "6 x 24 x 500ml",                           PriceValue = 25.83m,    PriceUnit = "GBP" , InventoryQuantity = 16,     Sku="DR-000003", CategoryId = "737c9710-e069-436a-a236-660e8277dedf" }
+                new { Id = "bf7b5f89-a569-4e3d-acb8-04a07e1e1130",Name = "Sparkling Mineral Water", Packaging = "6 x 24 x 500ml",                           PriceValue = 25.83m,    PriceUnit = "GBP" , InventoryQuantity = 16,     Sku="DR-000003", CategoryId = "737c9710-e069-436a-a236-660e8277dedf" },
+                
+                new { Id = "5e0b02d5-a25f-4617-9215-3c646f9b4ae8",Name = "Plain Flour",             Packaging = "10 x 1kg",                                 PriceValue = 6.21m,     PriceUnit = "GBP" , InventoryQuantity = 4,     Sku="CI-000001", CategoryId = "3786f39a-a229-4689-aed7-d851082cd87a" },
+                new { Id = "b0ad84dc-1df9-4e84-970e-d6e49dee87f3",Name = "Icing Sugar",             Packaging = "12 x 500g",                                PriceValue = 9.38m,     PriceUnit = "GBP" , InventoryQuantity = 6,     Sku="CI-000002", CategoryId = "3786f39a-a229-4689-aed7-d851082cd87a" },
+                new { Id = "89090eed-5f8d-44bd-ac60-af45256c92ec",Name = "Free-Range Eggs",         Packaging = "10 x 12 each",                             PriceValue = 9.52m,     PriceUnit = "GBP" , InventoryQuantity = 9,     Sku="CI-000003", CategoryId = "3786f39a-a229-4689-aed7-d851082cd87a" },
+                new { Id = "e337af6b-c746-4d04-9dac-f57cc52a6158",Name = "Caster Sugar",            Packaging = "16 x 750g",                                PriceValue = 12.76m,    PriceUnit = "GBP" , InventoryQuantity = 13,    Sku="CI-000004", CategoryId = "3786f39a-a229-4689-aed7-d851082cd87a" }
             );
             
             
